4c49f0d00180e6e27b0f3571701ae3d599140cf8
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-11-22 20:28:14 -0500

    Create main.cpp
    Initial check-in.
    Complexity is O(n) and got following results.
    
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Remove Element.
    Memory Usage: 9.3 MB, less than 23.09% of C++ online submissions for Remove Element.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        int index = 0;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                count++;
+                nums[index++] = nums[i];
+            }
+        }
+        return count;
+    }
+};
